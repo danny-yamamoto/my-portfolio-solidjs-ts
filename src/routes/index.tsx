5078c6d1ec75f6359bbe8180e7357dc3f7a34fba
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-09-18 07:25:10 +0000

    Add articles section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,25 @@
 import { useRouteData } from "@solidjs/router"
 import { createServerData$ } from "solid-start/server";
-import { getExperience } from "../utils/experience"
+import { getExperience } from "../utils/experience.server"
 import Introduction from "../components/Introduction";
 import Experience from "../components/Experience";
-import { IExperience, IIntroduction } from "../types"
+import { IArticles, IExperience, IIntroduction } from "../types"
+import Articles from "~/components/Articles";
+import { getArticles } from "../utils/articles.server";
 
 type CombinedJson = {
   displayExperience: any[];
+  displayArticles: any[];
 }
 
 export function routeData() {
   return createServerData$(async (_, { request }) => {
     const experience: any[] = await getExperience();
+    const articles = await getArticles(20);
 
     const combinedJson:CombinedJson = {
       displayExperience: experience,
+      displayArticles: articles,
     }
 
     return combinedJson;
@@ -31,10 +36,15 @@ export default function Home() {
     title: "Experience",
     detail: data()?.displayExperience,
   }
+  const iArticles: IArticles = {
+    title: "Top 20 Articles",
+    detail: data()?.displayArticles,
+  }
   return (
     <div>
       <Introduction introduction={iIntroduction} />
       <Experience experience={iExperience}/>
+      <Articles articles={iArticles}/>
     </div>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,8 @@ export interface IExperience {
     title: string;
     detail: any[] | undefined; // undefinedを許容
 }
+
+export interface IArticles {
+    title: string;
+    detail?: any[] | undefined;
+}
