6fb2720962da66f5942f71f360ae19559ac9f324
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-09-18 10:53:10 +0000

    Add repositories section.

---
+++ src/components/Repositories.tsx
@@ -0,0 +1,21 @@
+import { Component } from "solid-js";
+import { IArticles, IRepositories } from "../types";
+
+const Repositories: Component<{ repositories: IRepositories }> = props => {
+    return (
+        <section id="articles">
+            <h1>{props.repositories.title}</h1>
+            <div class="repo-container">
+                {props.repositories.detail && props.repositories.detail.map(({ name, description, url }) => (
+                    <div id={name} class="repo-tile">
+                        <h3 class="repo-name">{name}</h3>
+                        <p class="repo-description">{description}</p>
+                        <a target="_blank" href={url} id={name}>View on GitHub</a>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Repositories;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,24 +3,32 @@ import { createServerData$ } from "solid-start/server";
 import { getExperience } from "../utils/experience.server"
 import Introduction from "../components/Introduction";
 import Experience from "../components/Experience";
-import { IArticles, IExperience, IFooter, IIntroduction } from "../types"
+import { IArticles, IExperience, IFooter, IIntroduction, IRepositories } from "../types"
 import Articles from "~/components/Articles";
 import { getArticles } from "../utils/articles.server";
 import Footer from "~/components/Footer";
+import { getRepositories } from "~/utils/repositories.server";
+import Repositories from "~/components/Repositories";
 
 type CombinedJson = {
   displayExperience: any[];
   displayArticles: any[];
+  displayRepositories: any[];
 }
 
 export function routeData() {
   return createServerData$(async (_, { request }) => {
+    const vars: any = process.env;
+    const ghEndpoint: string = vars.GRAPHQL_API;
+    const ghToken: string = vars.GH_TOKEN;
     const experience: any[] = await getExperience();
     const articles = await getArticles(20);
+    const repositories = await getRepositories(ghEndpoint, ghToken);
 
     const combinedJson:CombinedJson = {
       displayExperience: experience,
       displayArticles: articles,
+      displayRepositories: repositories,
     }
 
     return combinedJson;
@@ -41,8 +49,12 @@ export default function Home() {
     title: "Top 20 Articles",
     detail: data()?.displayArticles,
   }
+  const iRepositories: IRepositories = {
+    title: "Repositories",
+    detail: data()?.displayRepositories,
+  }
   const iFooter:IFooter = {
-    titile: "Daisuke Yamamoto",
+    title: "Daisuke Yamamoto",
     github: "https://github.com/danny-yamamoto",
     twitter: "https://twitter.com/dai_s_a_n",
   }
@@ -51,6 +63,7 @@ export default function Home() {
       <Introduction introduction={iIntroduction} />
       <Experience experience={iExperience}/>
       <Articles articles={iArticles}/>
+      <Repositories repositories={iRepositories} />
       <Footer footer={iFooter} />
     </div>
   );
